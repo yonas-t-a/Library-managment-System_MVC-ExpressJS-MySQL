9f7b509d5c272f52ad5346634a3600306e76c6ae
yonas-t-a <yonas.tessema@aait.edu.et>
2024-12-29 19:18:31 +0300

    basic folder setup

---
+++ model/catagories.js
@@ -0,0 +1,13 @@
+const db = require('../config/database');
+
+/**
+ * Create catagories table
+ * 
+ * @CatagoryID {VARCHAR}
+ * @CatagoryName {VARCHAR}
+ * @Descrition {VARCHAR}
+ */
+
+const catagories = [
+    
+]
---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+const PORT =  process.env.PORT || 3500;
+
+
+app.listen(PORT ,  () => {
+    console.log(`The app listining at port ${PORT}`)
+})
