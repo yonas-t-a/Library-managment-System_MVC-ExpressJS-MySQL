6d75b9b40dbc987fd3c23206324ebd1376b0b4ca
yonas-t-a <yonas.tessema@aait.edu.et>
2024-12-30 13:47:24 +0300

    create a finction the will execute SQL file

--- database.js
+++ database.js
@@ -3,9 +3,21 @@ import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config()
 
+import fs from 'fs';
+
 const pool = mysql.createPool({
     host: process.env.MySQL_HOST,
     name: process.env.MySQL_NAME,
     password: process.env.MySQL_PASSWORD,
     multipleStatements: true
 }).promise();
+
+const executeSQLFile = async (filePath) => {
+    try {
+        const sql = fs.readFileSync(filePath);
+        await pool.query(sql)
+        console.log(`Query OK ${filePath}`)
+    } catch (error) {
+        console.log(`Error in Excuting ${filePath}: ${error.message}`)
+    }
+}
