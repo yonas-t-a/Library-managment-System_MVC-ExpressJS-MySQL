a125c04fd360af299253a99b8be4285cef1c32e8
yonas-t-a <yonas.tessema@aait.edu.et>
2024-12-30 11:10:17 +0300

    change the way to import admin route

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
-import adminRouter from './routers/api/adminRouters.js'
+import adminRouters from './routers/api/adminRouters.js'
 import bookRouters from './routers/api/bookRouters.js'
 
 const app = express();
 const PORT =  process.env.PORT || 3501;
 
-app.use('/api/adminstrator', adminRouter)
+app.use('/api/adminstrator', adminRouters)
 app.use('/book', bookRouters)
 
 
