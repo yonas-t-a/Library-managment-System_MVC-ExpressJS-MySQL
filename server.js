3a9aef5ebc72cddeeff2f1bb50ad86840f7f1ec2
yonas-t-a <yonas.tessema@aait.edu.et>
2024-12-30 12:14:49 +0300

    create a route for each api endpoints

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ import bookRouters from './routers/api/bookRouters.js'
 import branchRouters from './routers/api/branchesRouters.js'
 import catagoriesRouters from './routers/api/catagoriesRouters.js'
 import finesRouters from './routers/api/finesRouters.js'
+import membersRouters from './routers/api/membersRouters.js'
+import nonMemberBorrwerRouters from './routers/api/nonMemberBorrwerRouters.js'
+import reservationRouters from './routers/api/reservationRouters.js'
+import shelvesRouters from './routers/api/shelvesRouters.js'
+import staffRouters from './routers/api/staffRouters.js'
+import transactionRouters from './routers/api/transactionRouters.js'
 
 const app = express();
 const PORT =  process.env.PORT || 3501;
@@ -16,6 +22,12 @@ app.use('/book', bookRouters)
 app.use('/branch', branchRouters)
 app.use('/catagorie', catagoriesRouters)
 app.use('/fines', finesRouters)
+app.use('/member', membersRouters)
+app.use('/nonMember', nonMemberBorrwerRouters)
+app.use('/reservation', reservationRouters)
+app.use('/shelves', shelvesRouters)
+app.use('/staffs', staffRouters)
+app.use('/transaction', transactionRouters)
 
 
 app.listen(PORT ,  () => {
