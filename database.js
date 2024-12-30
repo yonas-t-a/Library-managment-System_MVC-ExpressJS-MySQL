5636215f1d12d8738a3c80ff9a4803cc37a7e5fd
yonas-t-a <yonas.tessema@aait.edu.et>
2024-12-30 13:23:08 +0300

    create a connection to the database

--- database.js
+++ database.js
@@ -1,1 +1,11 @@
+import mysql from 'mysql2';
 
+import dotenv from 'dotenv';
+dotenv.config()
+
+const pool = mysql.createPool({
+    host: process.env.MySQL_HOST,
+    name: process.env.MySQL_NAME,
+    password: process.env.MySQL_PASSWORD,
+    multipleStatements: true
+}).promise();
