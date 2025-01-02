274339c13fbc89b69f0a7abfb10554a63a7b25e3
yonas-t-a <yonas.tessema@aait.edu.et>
2025-01-02 09:30:23 +0300

    add arrtibute gating function of adminModel

--- model/admin.js
+++ model/admin.js
@@ -4,11 +4,28 @@ import IdGenerator from "./reusableFunctions/functionToCreateID.js";
 import getAllRows from "./reusableFunctions/getAlldataOfTable.js";
 import getRowByID from "./reusableFunctions/getRowBYID.js";
 import deleteRowByID from "./reusableFunctions/delateRowByID.js";
+import selectSpecificAttributesCell from "./reusableFunctions/selectSpecificCell.js";
 
 const tableName = 'admin'
 const idHoldingColumnName = 'adminID'
 
+
 const adminModel = {
+    adminName : async (id) => {
+        const result = await selectSpecificAttributesCell("name", tableName,idHoldingColumnName, id);
+        return result;
+    },
+    adminPhone_number: async (id)=>{
+        const result =  await selectSpecificAttributesCell("phone_number", tableName, idHoldingColumnName, id);
+        return result;
+    },
+    adminEmail: async (id) => {
+        const result = await selectSpecificAttributesCell("email", tableName, idHoldingColumnName, id);
+        return result;
+    },
+    adminPassword: async (id) => {
+        const result = await selectSpecificAttributesCell("password", tableName, idHoldingColumnName, id);
+    },
     insertAdmin: async (name, phone_number, email, password) => {
         const adminID = IdGenerator("adminID", "admin", "admin");
         const query = 'INSERT INTO admin (adminID ,name, phone_number, email, password) VALUES (?,?,?,?,?)'
@@ -19,10 +36,12 @@ const adminModel = {
         }
     },
     getAllAdmins: async () =>{
-        getAllRows(tableName)
+        const result = await getAllRows(tableName);
+        return result
     },
     getAdminByID: async(id)=>{
-        getRowByID(tableName, idHoldingColumnName, id)
+        const result = await getRowByID(tableName, idHoldingColumnName, id);
+        return result 
     },
     updateAdmin: async (id, name, phone_number, email, password) => {
         const query = 'UPDATE admin SET name=?, phone_number=?, email=?, password=? WHERE id=?'
@@ -34,7 +53,8 @@ const adminModel = {
 
     },
     deleteAdmin: async (id) => {
-        deleteRowByID(tableName, idHoldingColumnName, id)
+        const result = await deleteRowByID(tableName, idHoldingColumnName, id)
+        return result
     }
 }
 
