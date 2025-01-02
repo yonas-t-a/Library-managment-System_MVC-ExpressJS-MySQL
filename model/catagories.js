51753da692ab2b0a4b6b30db6005cab64ea2743b
yonas-t-a <yonas.tessema@aait.edu.et>
2025-01-02 10:06:49 +0300

    create more function to select specific cell of the

--- model/catagories.js
+++ model/catagories.js
@@ -3,11 +3,22 @@ import IdGenerator from "./reusableFunctions/functionToCreateID.js";
 import getAllRows from "./reusableFunctions/getAlldataOfTable.js";
 import getRowByID from "./reusableFunctions/getRowBYID.js";
 import deleteRowByID from "./reusableFunctions/delateRowByID.js";
+import selectSpecificAttributesCell from "./reusableFunctions/selectSpecificCell.js";
 
 const tableName = 'catagories';
 const idHoldingColumnName = 'catagoryID';
 
 const categoryModel = {
+    categoryName: async (id) => {
+        const result = await selectSpecificAttributesCell("catagoryName", tableName, idHoldingColumnName, id);
+        return result;
+    },
+
+    categoryDescription: async (id) => {
+        const result = await selectSpecificAttributesCell("description", tableName, idHoldingColumnName, id);
+        return result;
+    },
+
     insertCategory: async (catagoryName, description) => {
         const catagoryID = IdGenerator("catagoryID", "catagories", "category");
         const query = 'INSERT INTO catagories (catagoryID, catagoryName, description) VALUES (?,?,?)';
@@ -19,11 +30,13 @@ const categoryModel = {
     },
 
     getAllCategories: async () => {
-        await getAllRows(tableName);
+        const result = await getAllRows(tableName);
+        return result;
     },
 
     getCategoryByID: async (id) => {
-        await getRowByID(tableName, idHoldingColumnName, id);
+        const result = await getRowByID(tableName, idHoldingColumnName, id);
+        return result;
     },
 
     updateCategory: async (id, catagoryName, description) => {
@@ -36,8 +49,10 @@ const categoryModel = {
     },
 
     deleteCategory: async (id) => {
-        await deleteRowByID(tableName, idHoldingColumnName, id);
+        const result = await deleteRowByID(tableName, idHoldingColumnName, id);
+        return result;
     }
 };
 
 export default categoryModel;
+
